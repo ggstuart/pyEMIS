e4e12b44bbf77271699805a8ad245dce78adec45
Graeme Stuart <ggstuart@gmail.com>
2011-11-21 16:52:19 +0000

    Changing API. Deprecated current implementation of temperature and consumption methods in DataFactory. Added dataset3 method to indicate future progression. Interpolation now optionally calculates gaps and flags missing data beyond a given limit.

--- lib/DataAccess/factory.py
+++ lib/DataAccess/factory.py
@@ -2,111 +2,183 @@ import numpy as np
 from ..DataCleaning import cleaning, interpolation, utils
 import logging, time, datetime
 
+class FactoryError(Exception): pass
+class InvalidRequest(FactoryError): pass
+class UnknownColumnType(InvalidRequest): pass
+
 class DataFactory(object):
-  """object for generating datasets of the appropriate ndarray type for consumption data"""
-  def __init__(self, src):
-    self.dtype = np.dtype([('date', np.float), ('consumption', np.float), ('temperature', np.float)])
-    self.src = src()
+    """object for generating datasets of the appropriate ndarray type for consumption data"""
+    def __init__(self, src):
+        self.dtype = np.dtype([('date', np.float), ('consumption', np.float), ('temperature', np.float)])
+        self.src = src()
 
-  def temperature(self, meter_id, sd_limit=None, resolution=None, as_timestamp=False):
-    try:
-      date, integ, movement = self.temp_date, self.temp_integ, self.temp_movement
-      logging.debug('Loaded stored temperature data [%s]' % meter_id)
-    except AttributeError, e:
-      logging.debug('Loading temperature data [%s]' % meter_id)
-      self.temp_date, self.temp_integ, self.temp_movement = self.src.temperature(meter_id)
-      date, integ, movement = self.temp_date, self.temp_integ, self.temp_movement
-    date = utils.timestamp_from_datetime(date)
-    if sd_limit != None:
-        logging.info('Cleaning [%s]' % meter_id)
-        date, integ, movement = cleaning.clean_temp(date, integ, movement, sd_limit)
-    if resolution !=None:
-        logging.info('Interpolating [%s]' % meter_id)
-        date, integ, movement = interpolation.interpolate(date, integ, movement, resolution)
-    if not as_timestamp: date = utils.datetime_from_timestamp(date)
-    return date, integ, movement
+    def column(self, col_id, col_type, sd_limit=None, resolution=None, as_timestamp=False):
+        if col_type == 'integ':
+            return self.integ_column(col_id, sd_limit=sd_limit, resolution=resolution, as_timestamp=as_timestamp)
+        elif col_type == 'movement':
+            return self.movement_column(col_id, sd_limit=sd_limit, resolution=resolution, as_timestamp=as_timestamp)
+        else:
+            raise UnknownColumnType, "col_type argument must be either 'integ' or 'movement'. [%s] was passed." % col_type
 
-  def consumption(self, meter_id, sd_limit=None, resolution=None, as_timestamp=False):
-    logging.debug('Loading consumption data %s' % meter_id)
-    date, integ, movement = self.src.consumption(meter_id)
-    date = utils.timestamp_from_datetime(date)
-    if sd_limit != None:
-        logging.info('Cleaning [%s]' % meter_id)
-        date, integ, movement = cleaning.clean(date, integ, movement, sd_limit)
-    if resolution !=None:
-        logging.info('Interpolating [%s]' % meter_id)
-        date, integ, movement = interpolation.interpolate(date, integ, movement, resolution)
-    if not as_timestamp: date = utils.datetime_from_timestamp(date)
-    return date, integ, movement
+    def integ_column(self, col_id, sd_limit=None, resolution=None, as_timestamp=False):
+        dt, integ = self.src.timeseries(col_id)
+        movement = utils.movement_from_integ(integ)
+        ts = utils.timestamp_from_datetime(dt)
+        if sd_limit != None:
+            dt, integ, movement = cleaning.clean(dt, integ, movement, sd_limit)
+        if resolution !=None:
+            date, integ = interpolation.interpolate2(date, integ, resolution)
+            movement = utils.movement_from_integ(integ)
+        if not as_timestamp: date = utils.datetime_from_timestamp(date)
+        return date, integ
 
-  #TODO Make this generic so I can ask for any number of columns and just give the appropriate dataset id's
-  def dataset(self, cons_id, temp_id, sd_limit=30, temp_sd_limit=6, resolution=60*60*24):
-    logging.debug('constructing dataset')
-    temp_date, temp_integ, temp_movement = self.temperature(temp_id, temp_sd_limit, resolution, as_timestamp=True)
-    date, integ, movement = self.consumption(cons_id, sd_limit, resolution, as_timestamp=True)
-    _from = max(min(date), min(temp_date))
-    _to = min(max(date), max(temp_date))
-    cons_a = (date >= _from) & (date <= _to)
-    temp_a = (temp_date >= _from) & (temp_date <= _to)
-    date = date[cons_a]
-    integ = integ[cons_a]
-    temp_date = temp_date[temp_a]
-    temp_movement = temp_movement[temp_a]    
-    cons = utils.movement_from_integ(integ)
-    temp = temp_movement
-    size = len(cons)
-    result = np.array([(date[i+1], cons[i+1], temp[i+1]) for i in range(size-1)], dtype = self.dtype)
-    return result
+    def movement_column(self, col_id, sd_limit=None, resolution=None, as_timestamp=False):
+        dt, movement = self.src.timeseries(col_id)
+        integ = utils.integ_from_movement(movement)
+        ts = utils.timestamp_from_datetime(dt)
+        if sd_limit != None:
+            dt, integ, movement = cleaning.clean(dt, integ, movement, sd_limit)
+        if resolution !=None:
+            date, movement = interpolation.interpolate2(date, movement, resolution)
+            integ = utils.integ_from_movement(movement)
+        if not as_timestamp: date = utils.datetime_from_timestamp(date)
+        return date, movement
+        
+    def temperature(self, meter_id, sd_limit=None, resolution=None, as_timestamp=False, missing=False, limit=None):
+        logging.warning('Using deprecated interface')
+        date, integ, movement = self.src.temperature(meter_id)
+        date = utils.timestamp_from_datetime(date)
+        if sd_limit != None:
+            logging.debug('Cleaning [%s]' % meter_id)
+            date, integ, movement = cleaning.clean_temp(date, integ, movement, sd_limit)
+        if resolution !=None:
+            logging.debug('Interpolating [%s]' % meter_id)
+            date, movement = interpolation.interpolate2(date, movement, resolution, missing=missing, limit=limit)
+            integ = utils.integ_from_movement(movement)
+        if not as_timestamp: date = utils.datetime_from_timestamp(date)
+        return date, integ, movement
+
+    def consumption(self, meter_id, sd_limit=None, resolution=None, as_timestamp=False, missing=False):
+        logging.warning('Using deprecated interface')
+        logging.debug('Loading consumption data %s' % meter_id)
+        date, integ, movement = self.src.consumption(meter_id)
+        date = utils.timestamp_from_datetime(date)
+        if sd_limit != None:
+            logging.debug('Cleaning [%s]' % meter_id)
+            date, integ, movement = cleaning.clean(date, integ, movement, sd_limit)
+        if resolution !=None:
+            logging.debug('Interpolating [%s]' % meter_id)
+            date, integ = interpolation.interpolate2(date, integ, resolution)
+            movement = utils.movement_from_integ(integ)
+        if not as_timestamp: date = utils.datetime_from_timestamp(date)
+        return date, integ, movement
+
+    def dataset(self, cons_id, temp_id, sd_limit=30, temp_sd_limit=6, resolution=60*60*24):
+        logging.warning('Using deprecated interface for generating dataset')
+        temp_date, temp_integ, temp_movement = self.temperature(temp_id, temp_sd_limit, resolution, as_timestamp=True)
+        date, integ, movement = self.consumption(cons_id, sd_limit, resolution, as_timestamp=True)
+        _from = max(min(date), min(temp_date))
+        _to = min(max(date), max(temp_date))
+        cons_a = (date >= _from) & (date <= _to)
+        temp_a = (temp_date >= _from) & (temp_date <= _to)
+        date = date[cons_a]
+        integ = integ[cons_a]
+        temp_date = temp_date[temp_a]
+        temp_movement = temp_movement[temp_a]    
+        cons = utils.movement_from_integ(integ)
+        temp = temp_movement
+        size = len(cons)
+        result = np.array([(date[i+1], cons[i+1], temp[i+1]) for i in range(size-1)], dtype = self.dtype)
+        return result
     
   #{'id': 1, 'label': 'consumption', 'sd_limit': None, 'type': ['integ'|'movement']}
-  def dataset2(self, columns, resolution):
-    logging.debug('constructing dataset')
+    def dataset2(self, columns, resolution, missing=False):
+        logging.debug('constructing dataset')
 
-    #pic up the raw data
-    data = {}
-    for col in columns:
-        row = {'column': col}
-        if col['type'] == 'movement': row['date'], row['integ'], row['movement'] = self.temperature(col['id'], col['sd_limit'], resolution, as_timestamp=True)
-        elif col['type'] == 'integ': row['date'], row['integ'], row['movement'] = self.consumption(col['id'], col['sd_limit'], resolution, as_timestamp=True)
-        row['min_date'], row['max_date'] = min(row['date']), max(row['date'])
-        data[col['label']] = row
+        #pick up the raw data
+        data = {}
+        for col in columns:
+            row = {'column': col}
+            if col['type'] == 'movement': 
+                row['date'], row['integ'], row['movement'] = self.temperature(col['id'], col['sd_limit'], resolution, as_timestamp=True, missing=missing)
+            elif col['type'] == 'integ': 
+                row['date'], row['integ'], row['movement'] = self.consumption(col['id'], col['sd_limit'], resolution, as_timestamp=True, missing=missing)
+            row['min_date'], row['max_date'] = min(row['date']), max(row['date'])
+            data[col['label']] = row
 
-    #determine the range
-    _from = max([data[label]['min_date'] for label in data.keys()])
-    _to = min([data[label]['max_date'] for label in data.keys()])
+        #determine the range
+        _from = max([data[label]['min_date'] for label in data.keys()])
+        _to = min([data[label]['max_date'] for label in data.keys()])
 
-    #construct the result
-    result = {}
-    for col in columns:
-        label = col['label']
-#        result[label] = {}
-        a = (data[label]['date'] >= _from) & (data[label]['date'] <= _to)
-        if not result.has_key('date'): result['date'] = data[label]['date'][a]
-        if col['type'] == 'movement': result[label] = data[label]['movement'][a]
-        elif col['type'] == 'integ': result[label] = utils.movement_from_integ(data[label]['integ'][a])
+        #construct the result
+        result = {}
+        for col in columns:
+            label = col['label']
+            a = (data[label]['date'] >= _from) & (data[label]['date'] <= _to)
+            if not result.has_key('date'): result['date'] = data[label]['date'][a]
+            if col['type'] == 'movement': result[label] = data[label]['movement'][a]
+            elif col['type'] == 'integ': result[label] = utils.movement_from_integ(data[label]['integ'][a])
 
-    #convert to output
-    dt = np.dtype([(lbl, np.float) for lbl in result.keys()])
-    size = len(result['date'])
-    result = np.array([tuple([result[lbl][i+1] for lbl in result.keys()]) for i in xrange(size-1)], dtype = dt)
-#    result = np.array([(date[i+1], cons[i+1], temp[i+1]) for i in range(size-1)], dtype = self.dtype)
-    return result
+        #convert to output
+        dt = np.dtype([(lbl, np.float) for lbl in result.keys()])
+        size = len(result['date'])
+        result = np.array([tuple([result[lbl][i+1] for lbl in result.keys()]) for i in xrange(size-1)], dtype = dt)
+        return result
+
+    def dataset3(self, columns, resolution):
+        logging.debug('constructing dataset')
+
+        #pick up the raw data
+        data = {}
+        for c in columns:
+            row = {'column': c}
+            row['date'], row['value'] = self.column(c['id'], c['type'], sd_limit=c['sd_limit'], resolution=c['resolution'], as_timestamp=True)
+            row['min_date'], row['max_date'] = min(row['date']), max(row['date'])
+            data[c['label']] = row
+
+        #determine the range
+        _from = max([data[label]['min_date'] for label in data.keys()])
+        _to = min([data[label]['max_date'] for label in data.keys()])
+
+        #construct the result
+        result = {}
+        for c in columns:
+            label = c['label']
+            a = (data[label]['date'] >= _from) & (data[label]['date'] <= _to)
+            if not result.has_key('date'): result['date'] = data[label]['date'][a]
+            value = data[label]['value'][a]
+            if c['type'] == 'integ': value = utils.movement_from_integ(value)
+            result[label] = value
+
+        #convert to output
+        dt = np.dtype([(lbl, np.float) for lbl in result.keys()])
+        size = len(result['date'])
+        result = np.array([tuple([result[lbl][i+1] for lbl in result.keys()]) for i in xrange(size-1)], dtype = dt)
+        return result
 
 
-  def locations(self):
-    return self.src.locations()
+    def locations(self):
+        return self.src.locations()
 
-  def metersForLocation(self, id):
-    return self.src.metersForLocation(id)
+    def metersForLocation(self, id):
+        return self.src.metersForLocation(id)
 
-  def meters(self):
-    return self.src.meters()
-  def meter(self, meter_id):
-    return self.src.meter(meter_id)
+    def meters(self):
+        return self.src.meters()
         
+    def meter(self, meter_id):
+        return self.src.meter(meter_id)
+
 if __name__ == "__main__":
-  from classic import Classic
-  df = DataFactory(Classic)
-  d1 = df.dataset(1)
-  d2 = df.dataset(2)
-  
+    from DynamatPlus.dplusAdapter import DynamatPlus
+    df = DataFactory(DynamatPlus)
+    cons = {'id': 213, 'label': 'consumption', 'type': 'integ', 'sd_limit': 30}
+    temp = {'id': 840, 'label': 'temperature', 'type': 'movement', 'sd_limit': 6}
+    columns = [cons, temp]
+    resolution=60*60*24
+
+    d1 = df.dataset(213, 840, resolution=resolution)
+    d2 = df.dataset2([columns], resolution=resolution)
+
+    print d1-d2
+    
--- lib/DataCleaning/interpolation.py
+++ lib/DataCleaning/interpolation.py
@@ -1,19 +1,90 @@
 import numpy as np
-import math, utils
+import math, utils, logging
 
 def interpolate(date, integ, movement, resolution):
-  first, last = math.ceil(min(date)/resolution)*resolution, math.floor(max(date)/resolution)*resolution
-  new_date = np.arange(first, last, resolution)
-  new_integ = np.interp(new_date, date, integ)
-  new_movement = utils.movement_from_integ(new_integ)
-  return new_date, new_integ, new_movement
+    first, last = math.ceil(min(date)/resolution)*resolution, math.floor(max(date)/resolution)*resolution
+    new_date = np.arange(first, last, resolution)
+    new_integ = np.interp(new_date, date, integ)
+    new_movement = utils.movement_from_integ(new_integ)
+    return new_date, new_integ, new_movement
+
+def interpolate2(ts, value, resolution, missing=False, limit=None):
+    first, last = math.ceil(min(ts)/resolution)*resolution, math.floor(max(ts)/resolution)*resolution
+    new_ts = np.arange(first, last, resolution)
+    new_value = np.interp(new_ts, ts, value)
+    if missing:
+        if limit == None: limit = resolution*2
+        mymissing = np.array([False]*len(new_ts), dtype=bool)
+        mygaps = gaps(ts, limit)
+        for g in mygaps:
+            logging.info(g)
+            a = (new_ts >= g['from']) & (new_ts <=g['to'])
+            mymissing[a] = True
+        return new_ts, new_value, mymissing, mygaps
+    return new_ts, new_value
 
 def hh(date, integ, movement):
-  return interpolate(date, integ, movement, resolution=60*30)
+    return interpolate(date, integ, movement, resolution=60*30)
 
 def daily(date, integ, movement):
-  return interpolate(date, integ, movement, resolution=60*60*24)
+    return interpolate(date, integ, movement, resolution=60*60*24)
 
 def weekly(date, integ, movement):
-  return interpolate(date, integ, movement, resolution=60*60*24*7)
+    return interpolate(date, integ, movement, resolution=60*60*24*7)
 
+
+def gaps(ts, limit):
+    """
+    For a given array of timestamps and a given size limit
+    returns a list of gaps between timestamps which are greater than the limit
+    Contiguous gaps are merged
+    """
+    seconds = np.diff(ts)   #   n - 1
+    logging.debug(seconds)
+    over_limit = seconds > limit
+    _gap = {'from': None, 'to': None}
+    result = []
+    for i in xrange(len(seconds)):
+        if not over_limit[i]:   #No gap, close off the current gap if it exists
+            if _gap['to'] != None:   #If a current gap exists
+                logging.debug('Saving the current gap and resetting')
+                result.append(_gap)
+                _gap = {'from': None, 'to': None}
+        else:   #We have a gap, either append it to current or start a new one
+            if _gap['to'] != None:   #If a current gap exists
+                if _gap['to'] == ts[i]:  #if the 'current' gap can be appended directly
+                    logging.debug("Extend the current gap")
+                    _gap['to'] = ts[i+1] #then extend the 'current' gap
+                    logging.debug(_gap)
+                else: #if not then something is going wrong
+                    logging.warning("New gap doesn't append to 'current' gap")
+                    result.append(_gap)
+                    _gap = {'from': None, 'to': None}
+            else:   #if no 'current' gap exists then start one
+                logging.debug("starting a new gap")
+                _gap['from'] = ts[i] #just get the next data
+                _gap['to'] = ts[i+1] #and loop round to next i
+                logging.debug(_gap)
+    if _gap['to'] != None:   #If a current gap exists at the end it needs to be saved
+        logging.debug('Saving the current gap and resetting')
+        result.append(_gap)
+    return result
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    logging.basicConfig(level=logging.INFO)
+    ts = []
+    ts.extend(range(0  , 100, 10))
+    ts.extend(range(101, 200, 5))
+    ts.extend(range(201, 300, 2))
+    ts.extend(range(301, 400, 8))
+    ts.extend(range(401, 500, 5))
+    v = np.cumsum(np.random.rand(len(ts)))
+    ts2, v2, m, gaps = interpolate2(ts, v, 3, missing=True, limit=10)
+    plt.figure()
+    for g in gaps:
+        plt.axvspan(g['from'], g['to'], alpha=0.25, color='black')
+    plt.plot(ts, v, 'o--', color='blue', alpha = 0.5)
+    plt.plot(ts2, v2, 'x-', color='red')
+    plt.plot(ts2[m], v2[m], '+', color='black')
+    plt.show()
