0955d0b41c08ab4f6aec04667a037bbdb22d87e0
Graeme Stuart <ggstuart@gmail.com>
2012-02-03 17:10:09 +0000

    Renamed factory.py to dal.py and updated import in __init__.py

--- lib/DataAccess/__init__.py
+++ lib/DataAccess/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.1'
 __all__ = ['DataFactory', 'sources', 'adapters']#, 'Classic', 'CleanData', 'Fake', 'DynamatPlus']
 
 #The data factory provides nice, formatted datasets from a given source ready to analyse
-from factory import DataAccessLayer
+from dal import DataAccessLayer
 
 import sources
 import adapters
