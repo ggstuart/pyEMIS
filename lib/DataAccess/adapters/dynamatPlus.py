0b967d195cb008088aba559717609b585645b533
Graeme Stuart <ggstuart@gmail.com>
2012-02-16 21:10:49 +0000

    Extracting more information from the dynamat database - working with new expectations of DataAccessLayer

--- lib/DataAccess/adapters/dynamatPlus.py
+++ lib/DataAccess/adapters/dynamatPlus.py
@@ -2,6 +2,9 @@ from ..sources import DynamatPlus as DplusSource
 import numpy as np
 import calendar, os.path, logging
 
+class DynamatPlusError(Exception): pass
+   
+
 class DynamatPlus(object):
     def __init__(self, config_path=os.path.expanduser('~/.EMIS/config.cfg')):
         """
@@ -11,37 +14,68 @@ class DynamatPlus(object):
         <pyEMIS.DataAccess.adapters.dynamatPlus.DynamatPlus object at ...>
         >>>
         """
+        self.logger = logging.getLogger('DataAccess:adapters:DynamatPlus')
         self.src = DplusSource(config_path, "DynamatPlus")
 
-    def consumption(self, meter_id):
-        logging.debug('Getting meter_%04i from DynamatPlus' % meter_id)
-        data = self.src.readingsList(meter_id)
-        date = [d['Reading_DateTime'] for d in data]
-        integ =  [d['Reading_Or_Total'] for d in data]
-        movement =  [d['Delivered_Or_Movement'] for d in data]
-#        return self._convert_to_date(date), self._convert_to_float(integ), self._convert_to_float(movement)
-        return date, self._convert_to_float(integ), self._convert_to_float(movement)
-
-    def temperature(self, meter_id):
-        logging.debug('Getting temperature_%04i from DynamatPlus' % meter_id)
-        data = self.src.temperatureList(meter_id)
-        date = [d['Reading_DateTime'] for d in data]
-        integ =  [d['Reading_Or_Total'] for d in data]
-        movement =  [d['Delivered_Or_Movement'] for d in data]
-        return date, self._convert_to_float(integ), self._convert_to_float(movement)
-        
     def timeseries(self, meter_id):
         """
-        Retrieve data for a given timeseries
+        Retrieve readings for a given meter_id
         """
-        m = self.src.meter(meter_id)
-        if m['Meter_Type'] == 4:
-            return self.temperature(meter_id)
+        self.logger.debug('Getting meter %05i from DynamatPlus' % meter_id)
+        m = self.src.meter_with_units(meter_id)
+        self.logger.debug('meter type: %i' % m['type'])
+        self.logger.debug('unit type: %i' % m['unit_type'])
+        units = {'name': m['unit_description'].strip(), 'abbreviation': m['abbreviation'].strip()}
+        value_type = 'unknown'
+        if units['abbreviation'][0] == chr(0xb0):
+            #replace degree symbol - boo!
+            units['abbreviation'] = 'Degrees ' + units['abbreviation'][1:]
+            value_type = 'Temperature'
+            
+        if m['type'] == 1:
+            data_type = 'integ'
+            if m['unit_type'] == 0:
+                self.logger.debug('Energy data')
+                value_type = 'Consumption'
+                data = self.src.energyColumn(meter_id)
+                units = {'name': 'kiloWatt-hours', 'abbreviation': 'kWh'}
+            elif m['unit_type'] == 1:
+                self.logger.debug('Water data')
+                value_type = 'Consumption'
+                data = self.src.waterColumn(meter_id)
+                units = {'name': 'Cubic meters', 'abbreviation': 'm3'}
+            elif m['unit_type'] == 2:
+                self.logger.debug('%s data' % m['unit_description'])
+                data = self.src.integColumn(meter_id)
+            else:
+                raise DynamatPlusError, "Unknown unit type for integ meter (type 1) [%s]" % m['unit_type']
+
+        elif m['type'] == 4:
+            data_type = 'movement'
+            if m['unit_type'] == 2:
+                self.logger.debug('%s data' % m['unit_description'])
+                data = self.src.movementColumn(meter_id)
+            else:
+                raise DynamatPlusError, "Unknown unit type for movement meter (type 4) [%s]" % m['unit_type']
+
+        elif m['type'] == 6:
+            raise DynamatPlusError, "Virtual meter!"
         else:
-            return self.consumption(meter_id)
+            raise DynamatPlusError, "Unknown meter type [%s]" % m['type']
+
+        result = {
+            'description': m['description'],
+            'type': data_type,
+            'value_type': value_type,
+            'datetime': [d['datetime'] for d in data],
+            'timestamp': self._convert_to_date([d['datetime'] for d in data]),
+            'value': np.array([d['value'] for d in data], dtype=float),
+            'units': units
+        }
+        return result
 
     def meterInfo(self, meter_id):
-        return src.meterSummary(meter_id)
+        return sself.src.meter_with_units(meter_id)
 
     #convert sql date to numpy ndarray float
     def _convert_to_date(self, data):
@@ -49,14 +83,6 @@ class DynamatPlus(object):
         result = np.array(result, dtype=float)
         return result
 
-    #convert sql data to numpy ndarray float
-    def _convert_to_float(self, data):
-        return np.array(data, dtype=float)
-
-    def __getattr__(self, attr):
-        """Everything else is delegated to src"""
-        return getattr(self.src, attr)
-        
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
--- lib/DataAccess/sources/dynamatPlus.py
+++ lib/DataAccess/sources/dynamatPlus.py
@@ -26,7 +26,6 @@ class DynamatPlus(object):
         except Exception, e:
             self._logger.error(e)
             raise
-            
 
     #Prevent SQL injection by passing args directly to execute function rather than using string formatting stuff
     def _query(self, sql, *args):
@@ -53,19 +52,63 @@ class DynamatPlus(object):
         [{0: 111, 1: 'LEC/KIMB/EM01                 ', 2: 1, 'Description': 'LEC/KIMB/EM01                 ', 'meter_type': 1, 'Meter_ID': 111}]
         >>> 
         """
-        return self._query("SELECT Meter_ID, Description, meter_type FROM Meter WHERE meter_id = %s;", meter_id)
+        return self._query("SELECT Meter_ID as id, Description as description, meter_type as type FROM Meter WHERE meter_id = %s;", meter_id)[0]
 
-    def meterList(self):
-        return self._query("SELECT Meter_ID, Description FROM Meter;")
+    def meter_with_units(self, meter_id):
+        """
+        The meter method uses _query.
+        >>> import os.path, pyEMIS.DataAccess as DA, logging
+        >>> config_path = os.path.expanduser('~/.EMIS/config.cfg')
+        >>> src = DA.sources.DynamatPlus(config_path, "DynamatPlus")
+        >>> src.meter_with_units(111)
+        [{0: 111, 1: 'LEC/KIMB/EM01                 ', 2: 1, 'Description': 'LEC/KIMB/EM01                 ', 'meter_type': 1, 'Meter_ID': 111}]
+        >>> 
+        """
+        return self._query("SELECT m.Meter_ID as id, m.Description as description, m.meter_type as type, u.unit_description, u.unit_type, u.units_per_gj, u.units_per_cubic_metre, u.abbreviation FROM Meter m LEFT JOIN Units u ON m.measured_unit_id = u.unit_id WHERE meter_id = %s;", meter_id)[0]
 
-    def readingsList(self, meter_id):
-        return self._query("SELECT Reading_DateTime, Reading_Or_Total/Units_Per_GJ/0.0036 as Reading_Or_Total, Delivered_Or_Movement/Units_Per_GJ/0.0036 as Delivered_Or_Movement FROM Meter_Reading, Meter, Units WHERE Meter.Meter_ID = Meter_Reading.Meter_ID AND Units.Unit_ID=Meter.Measured_Unit_ID AND Meter.Meter_ID = '%s'", meter_id)
+#    def meterList(self):
+#        return self._query("SELECT Meter_ID, Description FROM Meter;")
 
-    def temperatureList(self, meter_id):
-        return self._query("SELECT Reading_DateTime, Reading_Or_Total, Delivered_Or_Movement FROM Meter_Reading, Meter WHERE Meter.Meter_ID = Meter_Reading.Meter_ID AND Meter.Meter_ID = '%s'", meter_id)
+#    def readingsList(self, meter_id):
+#        return self._query("SELECT Reading_DateTime, Reading_Or_Total/Units_Per_GJ/0.0036 as Reading_Or_Total, Delivered_Or_Movement/Units_Per_GJ/0.0036 as Delivered_Or_Movement FROM Meter_Reading, Meter, Units WHERE Meter.Meter_ID = Meter_Reading.Meter_ID AND Units.Unit_ID=Meter.Measured_Unit_ID AND Meter.Meter_ID = '%s'", meter_id)
 
-    def virtualMetersForSite(self, Site_ID):
-        return self._query("SELECT Meter.Meter_ID, Meter.Description FROM Meter LEFT JOIN Tree t1 ON Meter.Meter_ID = t1.Object_ID INNER JOIN Tree t2 ON t1.node_id = t2.parent_node_id WHERE t1.object_id = 75 AND t1.object_type = 16 AND t2.object_subtype = 6", str(Site_ID))
+#    def temperatureList(self, meter_id):
+#        return self._query("SELECT Reading_DateTime, Reading_Or_Total, Delivered_Or_Movement FROM Meter_Reading, Meter WHERE Meter.Meter_ID = Meter_Reading.Meter_ID AND Meter.Meter_ID = '%s'", meter_id)
+
+    def integColumn(self, meter_id):
+        """No unit correction, just the raw integ data"""
+        return self._query("SELECT Reading_DateTime as datetime, Reading_Or_Total as value FROM Meter_Reading WHERE Meter_ID = '%s'", meter_id)
+
+    def movementColumn(self, meter_id):
+        """No unit correction, just the raw movement data"""
+        return self._query("SELECT Reading_DateTime as datetime, Delivered_Or_Movement as value FROM Meter_Reading WHERE Meter_ID = '%s'", meter_id)
+
+    def energyColumn(self, meter_id):
+        """Convert units to kWh"""
+        return self._query("SELECT Reading_DateTime as datetime, Reading_Or_Total/Units_Per_GJ/0.0036 as value FROM Meter_Reading, Meter, Units WHERE Meter.Meter_ID = Meter_Reading.Meter_ID AND Units.Unit_ID=Meter.Measured_Unit_ID AND Meter.Meter_ID = '%s'", meter_id)
+
+    def waterColumn(self, meter_id):
+        """Convert units to m3"""
+        return self._query("SELECT Reading_DateTime as datetime, Reading_Or_Total/Units_Per_Cubic_Metre as value FROM Meter_Reading, Meter, Units WHERE Meter.Meter_ID = Meter_Reading.Meter_ID AND Units.Unit_ID=Meter.Measured_Unit_ID AND Meter.Meter_ID = '%s'", meter_id)
+
+    def childMeters(self, meter_id):
+        """
+        Returns a list of child meters for a given meter id
+        This is used to pull meter_ids for a virtual meter and should only be called if the parent meter_type = 6
+        """
+        sql = """
+            SELECT cNode.Virtual_Meter_Factor, cMeter.Meter_ID, cMeter.Description, cMeter.meter_type
+            FROM METER pMeter
+            INNER JOIN TREE pNode ON pMETER.METER_ID = pNODE.OBJECT_ID AND pNODE.Object_Type = 32
+            INNER JOIN TREE cNode ON pNODE.NODE_ID = cNODE.PARENT_NODE_ID
+            INNER JOIN METER cMETER ON cNODE.OBJECT_ID = cMETER.METER_ID AND cNODE.Object_Type = 32
+            WHERE
+            pMETER.METER_ID = %s
+        """
+        return self._query(sql, meter_id)
+
+#    def virtualMetersForSite(self, Site_ID):
+#        return self._query("SELECT Meter.Meter_ID, Meter.Description FROM Meter LEFT JOIN Tree parent ON Meter.Meter_ID = parent.Object_ID INNER JOIN Tree child ON parent.node_id = child.parent_node_id WHERE parent.object_id = 75 AND parent.object_type = 16 AND child.object_subtype = 6", str(Site_ID))
 
     def __del__(self):
         if self.conn:
