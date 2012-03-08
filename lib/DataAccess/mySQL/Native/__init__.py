20b8fb4d623c9873dd4ac202781da2f05cdd103e
Graeme Stuart <ggstuart@gmail.com>
2012-03-08 19:55:51 +0000

    removed cruft

--- lib/DataAccess/mySQL/Native/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = '0.1'
-__all__ = ['Native']
-
-from native import Native
-
--- lib/DataAccess/mySQL/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = '0.1'
-__all__ = ['MySQLSource', 'Classic', 'Native']
-
-from mySQLSource import MySQLSource
-from classic import Classic
-from Native import Native
