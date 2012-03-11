4e6316e3ae9f816e3cf506cc56fb3ec09752dc06
Graeme Stuart <ggstuart@gmail.com>
2012-03-11 00:08:08 +0000

    added doctest and moved code around

--- lib/DataAccess/dal.py
+++ lib/DataAccess/dal.py
@@ -10,11 +10,41 @@ class UnknownColumnType(DALError): pass
 
 class DataAccessLayer(object):
     """
-        Simple facade for all the complexities of data access
+    Simple facade for all the complexities of data access
+    >>> from pyEMIS.DataAccess import adapters, DataAccessLayer as DAL
+    >>> dal = DAL(adapters.Classic)
+    >>> dal #doctest: +ELLIPSIS
+    <pyEMIS.DataAccess.dal.DataAccessLayer object at 0x...>
+    >>> dal.adapter #doctest: +ELLIPSIS
+    <pyEMIS.DataAccess.adapters.classic.Classic object at 0x...>
+    >>> 
     """
     def __init__(self, adapter):
         self.logger = logging.getLogger('DataAccessLayer')
         self.adapter = adapter()
+
+    def column(self, col_id, sd_limit=None, resolution=None, as_timestamp=False):
+        """
+        Returns a column of data plus a few items of meta-data
+        The data are optionally cleaned and interpolated
+        'integ' data are converted to 'movement'
+        """
+        data = self.adapter.timeseries(col_id)
+        if sd_limit != None:
+            if data['type'] in ['consumption', 'integ']:
+                cleaner = ConsumptionCleaner(data)
+            elif data['type'] in ['temperature', 'movement']:
+                cleaner = TemperatureCleaner(data)
+            else:
+                raise UnknownColumnType, "I don't know what a '{0}' is.".format(data['type'])
+            data = cleaner.clean(sd_limit)
+        if resolution !=None:
+            interpolator = Interpolator(data)
+            data = interpolator.interpolate(resolution, missing=True)
+        if data['type'] == 'integ':
+            data['value'] = utils.movement_from_integ(data['value'])
+            data['type'] = 'movement'
+        return data    
 
     def dataset(self, columns, resolution):
         self.logger.debug('constructing dataset')
@@ -43,24 +73,6 @@ class DataAccessLayer(object):
         size = len(result['timestamp'])
         result = np.array([tuple([result[lbl][i+1] for lbl in result.keys()]) for i in xrange(size-1)], dtype = dt)
         return result
-
-    def column(self, col_id, sd_limit=None, resolution=None, as_timestamp=False):
-        data = self.adapter.timeseries(col_id)
-        if sd_limit != None:
-            if data['type'] in ['consumption', 'integ']:
-                cleaner = ConsumptionCleaner(data)
-            elif data['type'] in ['temperature', 'movement']:
-                cleaner = TemperatureCleaner(data)
-            else:
-                raise UnknownColumnType, "I don't know what a '{0}' is.".format(data['type'])
-            data = cleaner.clean(sd_limit)
-        if resolution !=None:
-            interpolator = Interpolator(data)
-            data = interpolator.interpolate(resolution, missing=True)
-        if data['type'] == 'integ':
-            data['value'] = utils.movement_from_integ(data['value'])
-            data['type'] = 'movement'
-        return data    
 
     def meter(self, meter_id):
         return self.adapter.meter(meter_id)
