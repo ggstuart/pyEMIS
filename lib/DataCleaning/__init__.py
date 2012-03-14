566154f70b852a4c8e2cdb2ae5174eb4d146d24a
Graeme Stuart <ggstuart@gmail.com>
2012-03-14 20:33:24 +0000

    Cleaned up another init file

--- lib/DataCleaning/__init__.py
+++ lib/DataCleaning/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '0.8'
 __all__ = ['ConsumptionCleaner', 'TemperatureCleaner', 'Interpolator']
 
 from cleaners import ConsumptionCleaner, TemperatureCleaner
 from interpolators import Interpolator
+
