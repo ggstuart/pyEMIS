9199044d8d13e0ced9f80f3c9abe7b403cba87c4
Graeme Stuart <ggstuart@gmail.com>
2012-11-08 11:08:03 +0000

    update dataset and datasource

--- lib/data/dataset.py
+++ lib/data/dataset.py
@@ -9,28 +9,33 @@ doesnt_need_integ = ['temperature']
 
 class Dataset(object):
     """Core Dataset class stores timeseries as numpy array and holds meta-data"""
-    def __init__(self, datetimes, values, unit, datasource):
+    def __init__(self, datetimes, values, datasource, unit=None):
         """
         takes datetimes and values with unit
         converts datetimes into a numpy array with timestamps
         converts values into base_units and stores base_units
         """
         assert(len(datetimes) == len(values))
-#        self.label = label
         self.datasource = datasource
         self.datasource.dataset = self
-#        self.commodity = commodity
-        self.unit = unit.base_unit
         dtype = np.dtype([('timestamp', np.float), ('value', np.float)])
         data = np.zeros(len(datetimes), dtype=dtype)
         data['timestamp'] = timestamp_from_datetime(datetimes)
         data['value'] = values
-        data['value'] = unit.to_base(data['value'])
+
+        #convert to specified unit or to the source data base unit
+        if unit:
+            self.unit = unit
+        else:
+            self.unit = self.datasource.unit.base_unit
+
+        data['value'] = self.datasource.unit.to_unit(data['value'], self.unit)
+
         self._processed = {}
         self._processed[(None, None)] = data
 
     def data(self, sd_limit=None, resolution=None, start=None, length=None):
-        """Provide access to data, optionally cleaned, optionally interpolated"""
+        """Provide access to data, optionally cleaned, optionally interpolated, optionally converted to another unit"""
             
         while True:
             try:
@@ -46,7 +51,7 @@ class Dataset(object):
                 except KeyError:
                     cleaner = cleaners.cleaner(self.datasource.commodity)
                     self._processed[(sd_limit, None)] = cleaner.clean(self._processed[(None, None)], sd_limit)
-        
+
         if not start:
             ts_start = min(data['timestamp'])
             start = datetime_from_timestamp(ts_start)
--- lib/data/datasource.py
+++ lib/data/datasource.py
@@ -1,4 +1,4 @@
-#Commodity should be mapped to a master list somewhere?
+from pyEMIS.data import DataError
 
 COMMODITIES = [
     'electricity',
@@ -9,85 +9,16 @@ COMMODITIES = [
     'water',
 ]
 
-class DataSourceError(Exception): pass
-
-class Node(object):
-    def __init__(self, label, parent=None, type_='Node'):
-        if parent:
-            self.parent = parent
-            self.parent.children.append(self)
-        self.label = label
-        self.type_ = type_
-        self.children = []
-
-    def __str__(self):
-        return "%s (%s)" % (self.label, self.type_)
-
-class Organisation(Node):
-    def __init__(self, label):
-        super(Organisation, self).__init__(label, type_='Oganisation')
-
-class Building(Node):
-    def __init__(self, label, organisation):
-        super(Building, self).__init__(label, parent=organisation, type_='Building')
-        self.organisation = self.parent
-        self.datasources = []
-        self.meters = self.datasources
+class DataSourceError(DataError): pass
 
 class DataSource(object):
     """Stores meta-data about the sensor, meter or simulation that generated a dataset"""
-    def __init__(self, label, commodity, node=None):
+    def __init__(self, label, commodity, unit):
         """
         Label should be a string that describes the data source
         """
         if commodity.lower() not in COMMODITIES:
             raise DataSourceError("Unknown commodity: %s (must be one of %s)" % (commodity.lower(), COMMODITIES))
-        if node:
-            self.node = node
-            node.datasources.append(self)
-        self.commodity = commodity.title()
+        self.commodity = commodity.lower()
         self.label = label
-
-class Meter(DataSource):
-    def __init__(self, label, commodity, building): 
-        super(Meter, self).__init__(label, commodity, node=building)
-        self.building = self.node
-        
-if __name__ == "__main__":
-
-#    home = Node('Home')
-
-    DMU = Organisation('DMU')#, parent=home)
-    LCC = Organisation('LCC')#, parent=home)
-
-    CC = Building('Campus Centre', DMU)
-    KMB = Building('Kimberlin Library', DMU)
-    NW = Building('New Walk', LCC)
-    JMCC = Building('Judgemeadow Community College', LCC)
-
-    shop = Node('Shop', parent=CC)
-    pub = Node('Pub', parent=CC)
-    office = Node('Office', parent=CC)
-
-    NWA = Node('A block', parent=NW)
-    NWB = Node('B block', parent=NW)
-    
-    JMElec = DataSource('JMCC electricity', 'energY', node=JMCC)
-    JMGas = DataSource('JMCC gas', 'eNergY', node=JMCC)
-    NWAGas = DataSource('NWA Gas', 'enerGY', node=NWA)
-    PubElec = DataSource('Pub electricity', 'enerGY', node=pub)
-
-
-    def print_node(node, tabs=1):
-        print "|%s%s" % (" -" * tabs, node)
-        new_tabs = tabs + 1
-        for datasource in node.datasources:
-            print "|%s%s" % (" ~" * new_tabs, datasource.label)
-        for child in node.children:
-            print_node(child, tabs=new_tabs)
-
-
-    print "|"
-    print_node(LCC)
-    print_node(DMU)
-    print "|"
+        self.unit = unit
