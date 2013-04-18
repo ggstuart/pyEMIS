5d2168ea4b76ab939bfa781ea170c28d6b83483e
Graeme Stuart <ggstuart@gmail.com>
2013-04-18 15:36:28 +0100

    removed reference to specific adapters and bumped version numbers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='pyEMIS',
-    version='0.9.1.dev0',
+    version='0.9.3.dev3',
     description='Python library for energy consumption data analysis',
     author='Graeme Stuart',
     author_email='ggstuart@gmail.com',
@@ -10,10 +10,6 @@ setup(
         'pyEMIS.data',
         'pyEMIS.data.preparation',
         'pyEMIS.data.adapters',
-        'pyEMIS.data.adapters.dynamat_plus',
-        'pyEMIS.data.adapters.sustainable_advantage',
-        'pyEMIS.data.adapters.british_gas',
-        'pyEMIS.data.adapters.graeme_phd',
         'pyEMIS.models',
         'pyEMIS.analysis',
     ],
