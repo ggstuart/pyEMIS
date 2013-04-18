05887dedf6934f62946c96834542656787bd371a
Graeme Stuart <ggstuart@gmail.com>
2013-04-18 15:33:45 +0100

    committing some changes I made ages ago. Tests probably broke a long time ago anyway

--- lib/test/unittest_dynamatplus.py
+++ lib/test/unittest_dynamatplus.py
@@ -1,48 +1,7 @@
 import unittest
 from pyEMIS.data.config import Config
 from pyEMIS.data.adapters import dynamat_plus as dp
-
-class testDynamatPlusSource(unittest.TestCase):
-    """A test class for the DynamatPlus data source"""
-    
-    def setUp(self):
-        conf = Config('Dynamat_DMU')
-        self.src = dp.Source(conf)
-
-    def testRootNode(self):
-        root = self.src.root()
-        self.assertEqual(root.Object_Description.strip(), 'Dynamat World')
-        top_level = root.children
-        for node in top_level:
-            self.assertEqual(node.parent, root)
-
-    def testSearch(self):
-        term = 'queens'
-        sites = self.src.siteSearch(term)
-        for site in sites:
-            self.assertIn(term.lower(), site.Name.strip().lower())
-        meters = self.src.meterSearch(term)
-        for meter in meters:
-            self.assertIn(term.lower(), meter.Description.strip().lower())
-
-    def testMeter(self):
-        meters = self.src.meterSearch('queens')
-        for m in meters[0:10]:
-            meter = self.src.meter(m.Meter_ID)
-            self.assertEqual(meter, m)
-
-    def testUnit(self):
-        unit = self.src.unit(1)
-        self.assertEqual(unit.Unit_Description.strip(), 'Cubic feet gas')
-        self.assertEqual(unit.Abbreviation.strip(), 'cu.ft')
-        self.assertEqual(float(unit.Units_Per_GJ), 928.31)
-
-    def testServiceType(self):
-        meters = self.src.meterSearch('queens')
-        for m in meters:
-            if m.Service_ID:
-                stype = self.src.service_type(m.Service_ID)
-                self.assertEqual(m.Service_Type, stype)
+from pyEMIS.data.unit import kWh, GJ
 
 class testDynamatPlusAdapter(unittest.TestCase):
     """A test class for the DynamatPlus data adapter"""
@@ -51,13 +10,22 @@ class testDynamatPlusAdapter(unittest.TestCase):
         conf = Config('Dynamat_DMU')
         self.adapter = dp.Adapter(conf)
 
+    def test_datasource(self):
+        qb = self.adapter.datasource(213)
+        self.assertEqual(qb.label, 'LEC/QUEN/ACD/ES01')
+        self.assertEqual(qb.commodity, 'electricity')
 
-    def testBasicData(self):
-        qb_elec = self.adapter.dataset("213")
-        self.assertGreater(len(qb_elec.data()), 10000)
-        self.assertEqual(qb_elec.unit.name, 'GigaJoules')
-        self.assertEqual(qb_elec.unit.suffix, 'GJ')
-        self.assertEqual(qb_elec.unit.coefficient, 1.0)
+    def test_unit(self):
+        unit = self.adapter.datasource(213).unit
+        base = unit.base_unit
+        self.assertEqual(base, GJ)
+
+#    def testBasicData(self):
+#        qb_elec = self.adapter.dataset("213")
+#        self.assertGreater(len(qb_elec.data()), 10000)
+#        self.assertEqual(qb_elec.unit.name, 'GigaJoules')
+#        self.assertEqual(qb_elec.unit.suffix, 'GJ')
+#        self.assertEqual(qb_elec.unit.coefficient, 1.0)
 
 
 if __name__ == "__main__":
--- lib/test/unittest_unit.py
+++ lib/test/unittest_unit.py
@@ -1,31 +1,34 @@
 import unittest
-from pyEMIS.data.unit import Unit, BaseUnit
+from pyEMIS.data.unit import Unit, BaseUnit, kWh, M3
 
 class testUnit(unittest.TestCase):
     """A test class for the Unit module"""
     
     def setUp(self):
-        self.joule = BaseUnit('Joule', 'J')
-        self.m3 = BaseUnit('Cubic Metre', 'm3')        
-        self.kWh = Unit(self.joule, 3600000.0, 'kiloWatt hour', 'kWh')
-        self.BTU = Unit(self.joule, 1055, 'British thermal unit', 'BTU')
-        self.litre = Unit(self.m3, 0.001, 'litre', 'l')
-        self.kilojoule = Unit(self.joule, 1000.0, 'KiloJoule', 'kJ')
+        self.GJ = BaseUnit('GigaJoules', 'GJ')
+        self.m3 = BaseUnit('Cubic Metres', 'm3')        
+        self.kWh = Unit(self.GJ, 0.0036, 'kiloWatt hours', 'kWh')
+        self.BTU = Unit(self.GJ, 0.000001055, 'British thermal units', 'BTU')
+        self.litre = Unit(self.m3, 0.001, 'litres', 'l')
+        self.kilojoule = Unit(self.GJ, 0.000001, 'KiloJoules', 'kJ')
 
     def testBasicAttributes(self):
-        self.assertEqual(self.joule.name, 'Joule')
-        self.assertEqual(self.joule.suffix, 'J')
-        self.assertEqual(self.kilojoule.base_unit, self.joule)
+        self.assertEqual(self.GJ.name, 'GigaJoules')
+        self.assertEqual(self.GJ.suffix, 'GJ')
+        self.assertEqual(self.kilojoule.base_unit, self.GJ)
 
     def testUnitConversion(self):
-        self.assertEqual(self.kilojoule.to_base(1.0), 1000.0)
+        self.assertEqual(self.kilojoule.to_base(1000.0), 0.001)
         self.assertEqual(self.kWh.to_unit(1.0, self.kilojoule), 3600.0)
-        self.assertEqual(self.kWh.to_base(1.0), 3600000.0)
+        self.assertEqual(self.kWh.to_base(1000.0), 3.6)
         
     def testInvalidConversion(self):
         self.assertRaises(TypeError, self.litre.to_unit, (1.0, self.kilojoule))
         self.assertRaises(TypeError, self.kilojoule.to_unit, (1.0, self.litre))
-        
+
+    def testEquivalence(self):
+        self.assertEqual(self.kWh, kWh)
+        self.assertEqual(self.m3, M3)
         
 if __name__ == "__main__":
     unittest.main()
