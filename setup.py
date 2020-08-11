1cdab1b2403b097ebac47ae68944e667619394fe
Graeme Stuart <ggstuart@gmail.com>
2020-08-11 15:30:42 +0100

    version bump
    ...and optimism regarding dependencies that used to fail to install 
    automatically

--- setup.py
+++ setup.py
@@ -2,19 +2,24 @@
 from __future__ import absolute_import, print_function
 from setuptools import setup, find_packages
 
-try:
-    import numpy
-    import scipy
-except ImportError:
-    raise ImportError("pyEMIS requires numpy and scipy")
+# try:
+#     import numpy
+#     import scipy
+# except ImportError:
+#     raise ImportError("pyEMIS requires numpy and scipy")
 
 setup(
     name='pyEMIS',
-    version='0.9.4',
+    version='0.9.5',
     description='Python library for energy consumption data analysis',
     author='Graeme Stuart',
     author_email='ggstuart@gmail.com',
     url='https://github.com/ggstuart/pyEMIS',
     packages=find_packages('lib'),
     package_dir={'': 'lib'},
+    install_requires=[
+        'numpy',
+        'scipy',
+        'python-dateutil'
+    ]
 )
